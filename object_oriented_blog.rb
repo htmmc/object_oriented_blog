3fa27083e380175d4ba191355105e7efead42b85
hitomim <hitomi.mzgk@gmail.com>
2015-01-13 16:30:42 -0500

    Initial framwork and Basic Blog class with front_page method

---
+++ object_oriented_blog.rb
@@ -0,0 +1,27 @@
+=begin 
+This is very simple. Implement a Blog class where you can add Post objects. Each post will have a title, a date and a text. 
+Then add a front_page method at Blog that returns the front page of the blog in the following format:
+
+Post title 1
+**************
+Post 1 text
+----------------
+Post title 2
+**************
+Post 2 text
+----------------
+Post title 3
+**************
+Post 3 text
+----------------
+=end 
+
+
+class Blog 
+	def front_page
+		puts"This is my blog"
+	end 
+end 
+
+blog = Blog.new
+blog.front_page 
