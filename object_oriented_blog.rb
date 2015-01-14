11b17d1e768cbeac5a5bc354069f249d948481d8
hitomim <hitomi.mzgk@gmail.com>
2015-01-13 19:11:59 -0500

    fixedd the error of unidentified name in 'front-page'

--- object_oriented_blog.rb
+++ object_oriented_blog.rb
@@ -15,44 +15,43 @@ Post title 3
 Post 3 text
 ----------------
 =end 
+
+
 class Blog
-	def initialize
-		
+	def initialize(bloglist)
+		@bloglist = bloglist
 	end 
 	def front_page
-		my_blogs = []
-		blog_array = my_blogs.map do |post|
-			Post.new post 
-		puts new_array
-		#Post_outputof each instance to be displayed
+		@bloglist.each {|blogpost| puts blogpost}
 	end 
 end 
 
-
-blog = Blog.new
-blog.front_page 
-
 class Post 
 	attr_reader :title, :text
+
 	def initialize (title, text)
 		@title = title
 		@text = text 
 		@date = Time.now()
 	end 
-	def post_output
-		puts "#{@title}: #{@date} \n**************\n#{@text}\n----------------" 
+
+	def post
+		"#{@title}: #{@date} \n**************\n#{@text}\n----------------" 
 	end 
-	#def post_sponsor
-	#	puts "******#{@title}******\n#{@text}\n----------------" 
-	#end 
+
+	def post_sponsor
+		"******#{@title}******\n#{@text}\n----------------" 
+	end 
 end 
 
 first_post = Post.new("This is my first blog!", "I am very happy to announce that I started my first blog today!")
-#first_post.post_output
-
+#first_post.post
 second_post =Post.new("Sponsor site: Want to learn how to code?", "We teach how to code in 8 weeks! Call now xxx-xxx-xxxx")
 #second_post.post_sponsor
 third_post =Post.new("My third date at Ironhack!", "My third day has not started yet but I think it is going to be intense!")
-#third_post.post_output
+#third_post.post
 
+my_blogs = [first_post.post, second_post.post_sponsor, third_post.post]
 
+blog = Blog.new(my_blogs)
+blog.front_page
